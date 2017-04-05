9ce6ed224a870b9c56f8a14e032b594926b8846d
O Morrill <owenpm3@1644952.no-reply.drupal.org>
2017-04-05 09:17:00 -0600

    Updates to main theme for use with bulletin.

--- profiles/express/themes/contrib/fiddlers_green/js/fiddlers_green.js
+++ profiles/express/themes/contrib/fiddlers_green/js/fiddlers_green.js
@@ -1,7 +1,7 @@
 // Change 'node' to 'form' before sending to UCB-bulletin
 
 (function ($) {
-  if ((location.pathname.indexOf('node') > -1) && !($(".user-logged-in")[0])) {
+  if ((location.pathname.indexOf('form') > -1) && !($(".user-logged-in")[0])) {
     jQuery('.tabs--primary').hide();
   }
 }(jQuery));
