e531225c97fd3c3386f14b3508135ab04d0a136f
O Morrill <owenpm3@1644952.no-reply.drupal.org>
2017-04-14 10:48:14 -0600

    Correct error from last commit--frontpage js.

--- profiles/express/themes/contrib/fiddlers_green/js/fiddlers_green.js
+++ profiles/express/themes/contrib/fiddlers_green/js/fiddlers_green.js
@@ -4,5 +4,5 @@
   if ((location.pathname.indexOf('form') > -1) && !($(".user-logged-in")[0])) {
     jQuery('.tabs--primary').hide();
   }
-  $('#edit-webform-submission-value-1--2.path-frontpage').attr('size', '17');
+  $('.path-frontpage #edit-webform-submission-value-1--2').attr('size', '17');
 }(jQuery));
