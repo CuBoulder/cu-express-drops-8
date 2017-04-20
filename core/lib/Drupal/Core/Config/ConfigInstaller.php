bf8a184b2cd3386b7942c764a11c8026bb22040b
Alex Finnarn <alexfinnarn@gmail.com>
2017-04-20 16:04:57 -0600

    hackingly commenting out fatal error...bwahahahaha

--- core/lib/Drupal/Core/Config/ConfigInstaller.php
+++ core/lib/Drupal/Core/Config/ConfigInstaller.php
@@ -480,7 +480,7 @@ class ConfigInstaller implements ConfigInstallerInterface {
 
     // Install profiles can not have config clashes. Configuration that
     // has the same name as a module's configuration will be used instead.
-    $profiles = $this->profileHandler->getProfiles();
+    //$profiles = $this->profileHandler->getProfiles();
     if (!isset($profiles[$name])) {
       // Throw an exception if the module being installed contains configuration
       // that already exists. Additionally, can not continue installing more
